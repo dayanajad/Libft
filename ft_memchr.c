05dc9b9ace5a625273b58911cbe2fcd2ca77a83d
Dayana <dayanajad97@gmail.com>
2025-05-20 17:15:19 +0800

    part 1 almost done

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbinti-m <dbinti-m@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 17:05:49 by dbinti-m          #+#    #+#             */
+/*   Updated: 2025/05/20 17:14:42 by dbinti-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t				i;
+	const unsigned char	*p;
+
+	p = (const unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (p[i] == (unsigned char)c)
+			return ((void *)&p[i]);
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbinti-m <dbinti-m@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 16:18:07 by dbinti-m          #+#    #+#             */
+/*   Updated: 2025/05/20 16:32:39 by dbinti-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int		i;
+	char	*last_found;
+
+	i = 0;
+	last_found = NULL;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			last_found = (char *)&s[i];
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	return (last_found);
+}
