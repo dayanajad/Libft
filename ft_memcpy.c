f506259024a492df0aff47730447606819654092
Dayana <dayanajad97@gmail.com>
2025-05-27 12:33:11 +0800

    updates

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dbinti-m <dbinti-m@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 10:02:40 by dbinti-m          #+#    #+#             */
-/*   Updated: 2025/05/22 21:28:28 by dbinti-m         ###   ########.fr       */
+/*   Updated: 2025/05/27 12:32:49 by dbinti-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
-	if (!dest && ! src)
+	if (!dest && !src && n > 0)
 		return (NULL);
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
