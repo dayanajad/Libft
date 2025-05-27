7090ac75bcf24606f0dd55fd93b120c421af8b30
Dayana <dayanajad97@gmail.com>
2025-05-27 11:18:29 +0800

    changess

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: dbinti-m <dbinti-m@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 21:29:08 by dbinti-m          #+#    #+#             */
-/*   Updated: 2025/05/26 22:27:29 by dbinti-m         ###   ########.fr       */
+/*   Updated: 2025/05/27 11:18:10 by dbinti-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
+	t_list	*tail;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = ft_lstnew(f(lst->content));
 	if (!new_list)
 		return (NULL);
+	tail = new_list;
 	lst = lst->next;
 	while (lst)
 	{
@@ -31,7 +33,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, new_node);
+		tail->next = new_node;
+		tail = new_node;
 		lst = lst->next;
 	}
 	return (new_list);
